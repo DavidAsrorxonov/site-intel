4ec5a25931ed2eababad1cd425fa080c5da6157d
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-05-16 17:14:16 +0900

    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+import "dotenv/config";
+import connectDB from "./config/db.js";
+
+connectDB();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
